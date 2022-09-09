0cb9cbfa5165ada07adfe5d4c66c0e9d49fab2bf
Nate Seilaff <seilaffn@gmail.com>
2022-09-08 21:10:41 -0600

    Add project files.

---
+++ BestBuyBestPractices/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Data;
+using System.IO;
+using MySql.Data.MySqlClient;
+using Microsoft.Extensions.Configuration;
+using DapperExercise1;
+//^^^^MUST HAVE USING DIRECTIVES^^^^
+
+var config = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+string connString = config.GetConnectionString("DefaultConnection");
+IDbConnection conn = new MySqlConnection(connString);
+
+var newConn = new DapperDepartmentsRepository(conn);
+
+var department = newConn.GetAllDepartments();
+
+foreach (var item in department)
+{
+    Console.WriteLine($"DepartmentID {item.DepartmentID} ");
+    Console.WriteLine($"Department Name {item.Name}");
+    Console.WriteLine();
+}
+
+Console.WriteLine("Create a new department:");
+var userIn = Console.ReadLine();
+newConn.AddDepartment(userIn);
+
